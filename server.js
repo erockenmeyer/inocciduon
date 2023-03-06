06598b7bede859c535a0ef52e70f27d20458ea0f
erockenmeyer <erikrockenmeyer@gmail.com>
2023-03-06 15:59:55 -0600

    adding server functionality

---
+++ server.js
@@ -0,0 +1,8 @@
+// add external techs
+const express = require('express');
+const session = require('express-session');
+const path = require('path')
+const exphbs = require('express-handlebars');
+
+// internal connections
+const routes = require('./controllers');
